b9de798de1f044438fb69cccb937672d87a49082
hyunsu_HP <hyunspookey@gmail.com>
2024-11-04 14:46:25 +0900

    single cell ca buffer turorial done

--- 10_Reaction-Diffusion/rxd_buffer/src/cfg.py
+++ 10_Reaction-Diffusion/rxd_buffer/src/cfg.py
@@ -3,7 +3,7 @@ from netpyne import specs
 cfg = specs.SimConfig()  # object of class cfg to store simulation configuration
 cfg.duration = 500  # Duration of the simulation, in ms
 cfg.recordTraces = {
-    "V_soma": {"sec": "soma", "loc": 0.5, "var": "v"},
+    # "V_soma": {"sec": "soma", "loc": 0.5, "var": "v"},
     "ca": {"sec": "soma", "loc": 0.5, "var": "cai"},
     "buf": {"sec": "soma", "loc": 0.5, "var": "bufi"},
     "cabuf": {"sec": "soma", "loc": 0.5, "var": "cabufi"},
@@ -15,10 +15,10 @@ cfg.recordStep = 0.1  # Step size in ms to save data (eg. V traces, LFP, etc)
 cfg.filename = "rxd_buffer"  # Set file output name
 cfg.analysis["plotTraces"] = {
     "include": ["cell"],
-    "oneFigPer": "cell",
-    # "ylim": [0, 0.0001],
+    # "oneFigPer": "cell",
+    "ylim": [0, 0.0001],
     "overlay": True,
     "saveFig": True,
 }  # Plot recorded traces for this list of cells
 
-cfg.analysis["plotRxDConcentration"] = {"speciesLabel": "ca", "regionLabel": "ecs"}
+# cfg.analysis["plotRxDConcentration"] = {"speciesLabel": "ca", "regionLabel": "cyt"}
--- 10_Reaction-Diffusion/rxd_buffer/src/init.py
+++ 10_Reaction-Diffusion/rxd_buffer/src/init.py
@@ -1,12 +1,21 @@
 from neuron import h
 from netpyne import sim
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # RUN MODEL
-#------------------------------------------------------------------------------
-cfg, netParams = sim.loadFromIndexFile('index.npjson')
+# ------------------------------------------------------------------------------
+cfg, netParams = sim.loadFromIndexFile("index.npjson")
 
-sim.create(netParams,cfg)
+sim.create(netParams, cfg)
 h.finitialize()
+
+# sim.net.createPops()  # instantiate network populations
+# sim.net.createCells()  # instantiate network cells based on defined populations
+# sim.net.connectCells()  # create connections between cells based on params
+# sim.net.addStims()  # add external stimulation to cells (IClamps etc)
+# sim.net.addRxD()  # add reaction-diffusion (RxD)
+# sim.setupRecording()  # setup variables to record for each cell (spikes, V traces, etc)
 sim.simulate()
 sim.analyze()
+
+# print(sim.net.params["rxdParams"])
